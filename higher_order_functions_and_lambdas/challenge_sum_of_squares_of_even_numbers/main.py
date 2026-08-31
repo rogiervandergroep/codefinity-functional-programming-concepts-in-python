34af852305d296697fa45f7db816bc911e8ccaf5
rogiervandergroep <>
2026-08-31 14:54:51 +0000

    Challenge: Sum of Squares of Even Numbers

---
+++ higher_order_functions_and_lambdas/challenge_sum_of_squares_of_even_numbers/main.py
@@ -0,0 +1,15 @@
+def sum_of_squares_of_evens(numbers):
+    evens = list(filter(lambda x: x % 2 == 0, numbers))
+    squares = list(map(lambda x: x * x, evens))
+    result = sum(squares)
+    return result
+
+
+result1 = sum_of_squares_of_evens([1, 2, 3, 4, 5, 6])
+print(result1)
+result2 = sum_of_squares_of_evens([7, 9, 11])
+print(result2)
+result3 = sum_of_squares_of_evens([0, -2, -4])
+print(result3)
+result4 = sum_of_squares_of_evens([])
+print(result4)

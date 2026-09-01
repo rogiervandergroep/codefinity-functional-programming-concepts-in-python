1165b2ce19d84e6893768d66c1796b6fb1f3d1cf
rogiervandergroep <>
2026-09-01 06:47:34 +0000

    Challenge: Modifying Enclosing Scope Variables

---
+++ variable_scope_nested_functions_and_closures/challenge_modifying_enclosing_scope_variables/main.py
@@ -0,0 +1,18 @@
+def bank_account():
+    balance = 1000
+    
+    def withdraw(amount):
+        nonlocal balance
+        if amount <= balance:
+            balance -= amount
+            return balance
+        else:
+            return 'Insufficient funds'
+
+    print(withdraw(200))
+    print(withdraw(900))
+    print(withdraw(100))
+
+
+
+bank_account()
